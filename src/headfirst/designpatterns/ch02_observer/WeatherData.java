760dfc8544cb14b915d28aed913b69ed081b82c2
Adam Scharf <scharfat@gmail.com>
2018-06-02 15:02:37 -0400

    Implement WeatherData

---
+++ src/headfirst/designpatterns/ch02_observer/WeatherData.java
@@ -0,0 +1,47 @@
+package headfirst.designpatterns.ch02_observer;
+
+import java.util.ArrayList;
+
+/**
+ * WeatherDa
+ */
+public class WeatherData implements Subject {
+    private ArrayList<Observer> observers;
+    private float temperature;
+    private float humidity;
+    private float pressure;
+    
+    public WeatherData() {
+        this.observers = new ArrayList<Observer>();
+    }
+
+    public void registerObserver(Observer o) {
+        this.observers.add(o);
+    }
+
+    public void removeObserver(Observer o) {
+        this.observers.remove(o);
+    }
+
+    public void notifyObservers() {
+        for (Observer observer : this.observers) {
+            observer.update(this.temperature, this.humidity, this.pressure);
+        }
+    }
+
+    public void measurementsChanged() {
+        this.notifyObservers();
+    }
+
+    /**
+     * Updates each of the sensors
+     * @param temperature the temperature to set
+     * @param humidity the humidity to set
+     * @param pressure the pressure to set
+     */
+    public void setMeasurements(float temperature, float humidity, float pressure) {
+        this.temperature = temperature;
+        this.humidity = humidity;
+        this.pressure = pressure;
+    }
+}
