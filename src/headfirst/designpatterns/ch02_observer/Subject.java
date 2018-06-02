5510694686ea382f23acff040834baf04efc57c6
Adam Scharf <scharfat@gmail.com>
2018-06-02 14:30:10 -0400

    Remove arraylist

--- src/headfirst/designpatterns/ch02_observer/Subject.java
+++ src/headfirst/designpatterns/ch02_observer/Subject.java
@@ -6,8 +6,6 @@ import java.util.ArrayList;
  * Subject
  */
 public interface Subject {
-    private observers ArrayList<Observer>;
-
     public void registerObserver(Observer o);
     public void removeObserver(Observer o);
     public void notifyObservers();
